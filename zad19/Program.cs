5f815cb469ab78ecc402b2480d9053fdba07bca0
levchakovd <levchakovd@yandex.ru>
2022-07-18 22:13:12 +0300

    палиндром

---
+++ zad19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+string k = Convert.ToString(n);
+
+
+if (k[0] == k[4] && k[1] == k[3])
+        Console.WriteLine("Да, это палиндром");
+
+else
+        Console.WriteLine("Нет");
