838593d3b152e3ddc3e699af406f24a52fd7b8d5
levchakovd <levchakovd@yandex.ru>
2022-07-19 03:08:28 +0300

    ura

---
+++ secmax/Program.cs
@@ -0,0 +1,23 @@
+﻿int [] n =  new int [] {1, 2, 3, 4, 5, 6, 7, 0, 8, 9, 10};
+int i = 0;
+int first=0;
+int second=0;
+
+
+while (n[i]>0)
+{
+    if (n[i]>first)
+    {
+    second = first;
+    first = n[i];
+    i++;
+    }
+
+    
+    else if (n[i]>second)
+    {
+        second = n[i];
+        i++;
+    }
+}
+Console.WriteLine(second);
